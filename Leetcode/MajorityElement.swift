605399cd24a79a85f7509158fd78a68e1d7d995e
Jack Camas <jackcamasdev@gmail.com>
2023-09-02 21:24:08 -0400

    Solved Majority Element

---
+++ Leetcode/MajorityElement.swift
@@ -0,0 +1,40 @@
+/*
+ 169. Majority Element
+ 
+ Given an array nums of size n, return the majority element.
+ The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+  
+
+ Example 1:
+ Input: nums = [3,2,3]
+ Output: 3
+ 
+ Example 2:
+ Input: nums = [2,2,1,1,1,2,2]
+ Output: 2
+ */
+
+class Solution {
+	func majorityElement(_ nums: [Int]) -> Int {
+		var dict = [Int:Int]()
+		
+		for i in 0..<nums.count {
+			if let count = dict[nums[i]] {
+				dict[nums[i]] = count + 1
+			} else {
+				dict[nums[i]] = 1
+			}
+		}
+
+		var majority = 0
+		var highestValue = 0
+		for (key, value) in dict {
+			if value > highestValue {
+				highestValue = value
+				majority  = key
+			}
+		}
+		return majority
+	}
+}
