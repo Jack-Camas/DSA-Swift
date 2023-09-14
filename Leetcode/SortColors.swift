a24f1f974abdab917a4a65db34a66f3824b1673e
Jack Camas <jackcamasdev@gmail.com>
2023-09-13 23:22:54 -0400

    Solved Sort Colors number 75 Leetcode

---
+++ Leetcode/SortColors.swift
@@ -0,0 +1,47 @@
+/*
+ 75. Sort Colors
+
+ Given an array nums with n objects colored red, white, or blue, sort them
+ in-place so that objects of the same color are adjacent, with the colors
+ in the order red, white, and blue.
+
+ We will use the integers 0, 1, and 2 to represent the color red, white,
+ and blue, respectively.
+
+ You must solve this problem without using the library's sort function.
+
+ Example 1:
+ Input: nums = [2,0,2,1,1,0]
+ Output: [0,0,1,1,2,2]
+ 
+ Example 2:
+ Input: nums = [2,0,1]
+ Output: [0,1,2]
+ */
+
+class Solution {
+	func sortColors(_ nums: inout [Int]) {
+	  var count1 = 0
+	  var count2 = 0
+
+	  for i in 0..<nums.count {
+		if nums[i] == 0 {
+		  count1 += 1
+		} else if nums[i] == 1 {
+		  count2 += 1
+		}
+	  }
+
+	  for i in 0..<count1 {
+		nums[i] = 0
+	  }
+
+	  for i in count1..<(count1 + count2) {
+		nums[i] = 1
+	  }
+
+	  for i in (count1 + count2)..<nums.count {
+		nums[i] = 2
+	  }
+	}
+}
