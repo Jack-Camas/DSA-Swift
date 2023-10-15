b5b8c2a9a7f2417659b1d37c97fa040123fc4765
Jack Camas <jackcamasdev@gmail.com>
2023-10-15 17:36:23 -0400

    Solved 383.RansomNote

---
+++ Leetcode/383.RansomNote.swift
@@ -0,0 +1,48 @@
+/*
+ 383. Ransom Note
+
+ Given two strings ransomNote and magazine, return true if ransomNote 
+ can be constructed by using the letters from magazine and false otherwise.
+
+ Each letter in magazine can only be used once in ransomNote.
+
+ Example 1:
+ Input: ransomNote = "a", magazine = "b"
+ Output: false
+ 
+ Example 2:
+ Input: ransomNote = "aa", magazine = "ab"
+ Output: false
+ 
+ Example 3:
+ Input: ransomNote = "aa", magazine = "aab"
+ Output: true
+ */
+
+class Solution {
+	func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+		var ransomDict = countChars(string: ransomNote)
+		var magDict = countChars(string: magazine)
+
+		func countChars(string: String) -> [Character:Int] {
+			var dict = [Character:Int]()
+			for c in string {
+				if let value = dict[c] {
+					dict[c] = value + 1
+				} else {
+					dict[c] = 1
+				}
+			}
+			return dict
+		}
+
+		for (char,count) in ransomDict {
+			if let value = magDict[char], count <= value {
+				continue
+			} else {
+				return false
+			}
+		}
+		return true
+	}
+}
